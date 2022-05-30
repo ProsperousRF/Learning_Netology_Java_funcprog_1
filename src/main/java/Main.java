9f86b0ade8bfe5464b87d229b46e6a76499c4fbc
Stanislav Rakitov <prosperousrf@gmail.com>
2022-05-30 23:58:35 +0300

    Генератор словаря

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * @author Stanislav Rakitov
+ */
+public class Main {
+  public static void main(String[] args) {
+    // Строка для словаря
+    String lorem =
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea est "
+            + "accusantium nostrum error omnis molestias hic illum aliquid officiis inventore "
+            + "deleniti a ducimus, officia sunt voluptatem rerum dignissimos quam vel?";
+
+    Function<String, List<String>> dictionary =
+        (s) -> Arrays.stream(s.split("[ .,?]+")).sorted().distinct().toList();
+
+    dictionary.apply(lorem).forEach(System.out::println);
+  }
+}
